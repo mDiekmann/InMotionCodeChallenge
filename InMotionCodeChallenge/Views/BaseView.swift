29ab727c2fda0d7577eaa3461c68c6429af13f42
Michael Diekmann <michael@gizmoapp.co>
2021-10-07 20:17:16 -0700

    Base view and view controller added

---
+++ InMotionCodeChallenge/Views/BaseView.swift
@@ -0,0 +1,32 @@
+//
+//  BaseView.swift
+//  InMotionCodeChallenge
+//
+//  Created by Michael Diekmann on 10/7/21.
+//
+
+import UIKit
+
+class BaseView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        setViews()
+        layoutViews()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+
+        setViews()
+        layoutViews()
+    }
+
+    // setup view UI (Text, font, coloring, etc.) and add subviews
+    func setViews() {}
+
+    // set constraints and layout subviews
+    func layoutViews() {}
+
+}
